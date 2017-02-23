c1350de86d0dc0c7642ab40f829756bc8c1b4712
Wojciech Łopata <wlopata@gmail.com>
2017-02-23 14:57:16 +0100

    display hardcoded search results

--- client/app/main/main.component.js
+++ client/app/main/main.component.js
@@ -5,11 +5,64 @@ import routing from './main.routes';
 export class MainController {
   awesomeThings = [];
   newThing = '';
+  matchingProducts = [
+    {
+      name: 'Masło Extra',
+      price: 5.45,
+      id: 42
+    },
+    {
+      name: 'Masło Extra Nieświeże Mix 2',
+      price: 5.45,
+      id: 46
+    },
+    {
+      name: 'Masło Extra',
+      price: 5.45,
+      id: 49
+    },
+    {
+      name: 'Masło Extra',
+      price: 5.45,
+      id: 142
+    },
+    {
+      name: 'Masło Extra',
+      price: 12.99,
+      id: 242
+    },
+    {
+      name: 'Masło Extra',
+      price: 5.45,
+      id: 542
+    },
+    {
+      name: 'Masło Extra',
+      price: 5.45,
+      id: 842
+    },
+    {
+      name: 'Masło Extra',
+      price: 5.45,
+      id: 12342
+    },
+    {
+      name: 'Masło Extra',
+      price: 455.45,
+      id: 42213
+    },
+    {
+      name: 'Masło Extra',
+      price: 5.45,
+      id: 42124
+    },
+  ];
 
   /*@ngInject*/
-  constructor($http, $scope, socket) {
+  constructor($http, $scope, socket, ngCart) {
     this.$http = $http;
     this.socket = socket;
+    this.ngCart = ngCart;
 
     $scope.$on('$destroy', function() {
       socket.unsyncUpdates('thing');
@@ -35,6 +88,15 @@ export class MainController {
 
   deleteThing(thing) {
     this.$http.delete(`/api/things/${thing._id}`);
+  }
+
+  cartIncrement(product) {
+    console.log('increment ' + product);
+    //this.ngCart.addItem('asdsa', 'adsadsd', 4.99, 2, {});
+  }
+
+  cartDecrement(product) {
+    console.log('decrement ' + product);
   }
 }
 
